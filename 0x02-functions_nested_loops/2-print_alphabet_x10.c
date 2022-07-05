22fd9e9d9889ee8680ec29af0633ef1426854b82
siqnax <siqnax@gmail.com>
2022-07-05 09:27:33 -0700

    error fixing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,8 @@
 
 /**                                       
 * print_alphabet_x10 - a function that prints 10 times the alphabet                 *
- * Return: x10 a-z
- */                                       
+* Return: x10 a-z
+*/                                       
 void print_alphabet_x10(void)
 {
 int n, co;
@@ -12,7 +12,7 @@ while (co < 10)
 {
 for (n = 'a'; n <= 'z'; n++)
 {
- _putchar(n);
+_putchar(n);
 }
 co++;
 _putchar('\n');
