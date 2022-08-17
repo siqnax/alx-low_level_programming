81ce88281eeead0952a17b9f3bcf55a007b87432
siqnax <siqnax@gmail.com>
2022-08-17 14:03:48 -0700

    error fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,37 @@
 #include "main.h"
+
 /**
- * times_table -  a function that prints the 9 times table, starting with 0
- * rone = row, cone = column, d = digits of current result
- * Return: times table
- * add extra space past single digit
+ * times_table - prints the 9 times table, starting with 0
  */
+
 void times_table(void)
 {
-int rone, cone, d;
+	int x, y, z;
 
-for (rone = 0; rone <= 9; rone++)
-{
-_putchar('0');
-_putchar(',');
-_putchar(' ');
-for (cone = 1; cone <= 9; cone++)
-{
-d = (rone * cone);
-if ((d / 10) > 0)
-{            _putchar((d / 10) + '0');
-}
-else
-{                _putchar(' ');
-}            _putchar((d % 10) + '0');
-if (cone < 9)
-{                _putchar(',');            _putchar(' ');
-}
-}
-_putchar('\n');
-}
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = x * y;
+			if (y == 0)
+			{
+				_putchar(z + '0');
+			}
+			if (z < 10 && y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
