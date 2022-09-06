91544bb6cee7b4a13c7f153c97a7e841ace5595e
siqnax <siqnax@gmail.com>
2022-09-05 21:51:10 -0700

    Task solution

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+./gm 9 8 10 24 75 9
